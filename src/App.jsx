4848623c524f65ab4767b7b5cfc285dedf147707
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-03-11 10:00:42 +0530

    added intial nodes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Flow from "./Flow";
+
+export default function App() {
+  return (
+    <div style={{ height: "97vh" }}>
+      <Flow />
+    </div>
+  );
+}
---
+++ src/nodes.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Input Node" },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: "2",
+    data: { label: 'Default Node' },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "3",
+    type: "output",
+    data: { label: "Output Node" },
+    position: { x: 250, y: 250 },
+  },
+];
